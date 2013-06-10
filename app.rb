4fe519b8a26c50b4bf1b7f8d8557633dab77d2eb
beechnut <cloydster@gmail.com>
2013-06-09 23:25:26 -0400

    add Alert model for Redis, add layouts

--- app.rb
+++ app.rb
@@ -9,26 +9,33 @@ require 'json'
 
 require 'rss'
 
-=begin
-# Configure redis
 
-configure do
-  services = JSON.parse(ENV['VCAP_SERVICES'])
-  redis_key = services.keys.select { |svc| svc =~ /redis/i }.first
-  redis = services[redis_key].first['credentials']
-  redis_conf = { :host => redis['hostname'], :port => redis['port']. :password => redis['password'] }
-  @@redis = Redis.new redis_conf
-  r = Redis::Namespace.new(:ns, :redis => @r)
+class Alert
+  attr_reader :id, :message, :tweeted, :link
+
+  def initialize(*args)
+    args.each do |arg|
+      arg.each_pair do |k,v|
+        instance_variable_set("@#{k}", v) unless v.nil?
+      end
+    end
+  end
+
+  def save
+    save_string = "'#{self.class.to_s.downcase}:#{id}'"
+    self.instance_variables.each do |var|
+      next if var == :@id  # Skip if it's the ID
+      save_string << ", '#{var.to_s.delete "@" }'"
+      save_string << ", \"#{self.instance_variable_get(var)}\""
+    end
+    puts "Still to-do: get this to save to redis"
+    puts "redis.hmset(#{save_string})"
+  end
 end
-=end
 
-=begin
-# Establish Twitter authorization
+a = Alert.new(id: 12, message: "This is the tweet about Government Center's Green Line delay.", tweeted: false, link: "http://bitly.com/sjF9F0A")
+a.save
 
-use OmniAuth::Builder do
-  provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
-end
-=end
 
 class App < Sinatra::Application
 
@@ -43,5 +50,14 @@ class App < Sinatra::Application
   get "/" do
     haml :index
   end
+
+  get "/:id" do
+    haml :show
+  end
+
+  not_found do  
+    status 404  
+    "404'd! There's no page here!"
+  end  
 
 end
---
+++ worker.rb
@@ -0,0 +1,6 @@
+# Tweeter
+# Worker job that gets alerts, compares them, saves & tweets new ones
+
+class Comparer
+
+end
